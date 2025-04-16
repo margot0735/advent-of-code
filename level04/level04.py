2ebd8f0d938937aa5ec0d95f3ac7cf21ecbd8bbd
Margot <margot.guillouard@gmail.com>
2025-04-16 23:22:26 +0200

    fix: fix implementation of day 4 part1

--- level04/level04.py
+++ level04/level04.py
@@ -10,30 +10,20 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.tools import parse_file
 
 #--- Main functions --- #
-def parse_file(input_path) -> list:
-    """function for reading txt file and returning the text in a list"""
-
-    verify_file(input_path)
-
+def read_input(input_path):
     with open(input_path, 'r') as file:
         input_list = file.read().strip().splitlines()
+    return [list(row.strip()) for row in input_list]
 
-    return [list(row) for row in input_list]
-
-
-def level04_part1(input_path) -> int:
-    word_search = parse_file(input_path)
-    word_search = [line.strip() for line in word_search if line.strip()]  #clean each line 
-    
+def level04_part1(input_path) -> int:  
     word = "XMAS"
+    word_length = len(word)
+    word_search = read_input(input_path)
     rows = len(word_search)
     cols = len(word_search[0])
     word_count = 0
 
-    directions = [(1, 0),(-1, 0),(0, 1),(0, -1),(1, 1),(-1, -1),(-1, 1),(1, -1)]
-
     def matches(i, j, dx, dy):
-        word_length = len(word)
         for k in range(word_length):
             x = i + k*dx
             y = j + k*dy
@@ -43,11 +33,12 @@ def level04_part1(input_path) -> int:
 
     for i in range(rows):
         for j in range(cols):
+            directions = [(1, 0),(-1, 0),(0, 1),(0, -1),(1, 1),(-1, -1),(-1, 1),(1, -1)]
             for dx, dy in directions:
                 if matches(i, j, dx, dy):
                     word_count += 1
 
-    return 0
+    return word_count
 
 
 # #--- Utility functions --- #
@@ -63,7 +54,7 @@ def level04_part2(input_path) -> int:
 #--- Main call --- #
 def main():
     file_path = "level04_input.txt"
-    result_part1 = level04_part1(parse_file(input_path))
+    result_part1 = level04_part1(file_path)
     #result_part2 = level04_part2(file_path)
     print("number of word found:", result_part1) 
     #print("":", result_part2)
--- tests/test_level04.py
+++ tests/test_level04.py
@@ -10,26 +10,25 @@ from level04.level04 import level04_part1, level04_part2
 def test_full_example(tmp_path):
     # Test example given in the advent of code"""
 
-    input_data_part1 = """
-    ....XXMAS.
-    .SAMXMS...
-    ...S..A...
-    ..A.A.MS.X
-    XMASAMX.MM
-    X.....XA.A
-    S.S.S.S.SS
-    .A.A.A.A.A
-    ..M.M.M.MM
-    .X.X.XMASX
+    input_data_part1 = """MMMSXXMASM
+    MSAMXMSMSA
+    AMXSXMAAMM
+    MSAMASMSMX
+    XMASAMXAMM
+    XXAMMXXAMA
+    SMSMSASXSS
+    SAXAMASAAA
+    MAMMMXMMMM
+    MXMXAXMASX
     """
     input_file_part1 = tmp_path / "input_part1.txt"
     input_file_part1.write_text(input_data_part1)
     assert level04_part1(str(input_file_part1)) == 18
 
-    # input_data_part2 = 
-    # input_file_part2 = tmp_path / "input_part2.txt"
-    # input_file_part2.write_text(input_data_part2)
-    # assert level04_part2(str(input_file_part2)) == 48
+    input_data_part2 = """ABA"""
+    input_file_part2 = tmp_path / "input_part2.txt"
+    input_file_part2.write_text(input_data_part2)
+    assert level04_part2(str(input_file_part2)) == 0
 
 # Launch tests with pytest
 if __name__ == "__main__":
