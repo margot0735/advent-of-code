2cd7af76aa6e40b7f099292f2fa7aa96125abcdc
Margot <margot.guillouard@gmail.com>
2025-04-16 23:42:41 +0200

    doc: documente level04_part1 function and word_is_found function

--- level04/level04.py
+++ level04/level04.py
@@ -10,45 +10,49 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.tools import parse_file
 
 #--- Main functions --- #
-def read_input(input_path):
-    with open(input_path, 'r') as file:
-        input_list = file.read().strip().splitlines()
-    return [list(row.strip()) for row in input_list]
 
-def level04_part1(input_path) -> int:  
+def level04_part1(input_path) -> int: 
+
+    # clean input file 
+    input_list = parse_file(input_path).strip().splitlines()        # create a list of strings, each row is a string
+    word_search = [list(row.strip()) for row in input_list]         # create a matrix of characters
+
     word = "XMAS"
-    word_length = len(word)
-    word_search = read_input(input_path)
     rows = len(word_search)
     cols = len(word_search[0])
     word_count = 0
-
-    def matches(i, j, dx, dy):
-        for k in range(word_length):
-            x = i + k*dx
-            y = j + k*dy
-            if not (0 <= x < rows and 0 <= y < cols) or (word_search[x][y]!=word[k]):
-                return False
-        return True
+    directions = [(1, 0),(-1, 0),(0, 1),(0, -1),(1, -1),(-1, 1),(-1, -1),(1, 1)]
 
     for i in range(rows):
         for j in range(cols):
-            directions = [(1, 0),(-1, 0),(0, 1),(0, -1),(1, 1),(-1, -1),(-1, 1),(1, -1)]
             for dx, dy in directions:
-                if matches(i, j, dx, dy):
+                if word_is_found(word_search, word, i, j, dx, dy):
                     word_count += 1
 
     return word_count
 
 
-# #--- Utility functions --- #
-
-
-
-
 def level04_part2(input_path) -> int:
     word_count = 0
     return word_count
+
+
+
+# #--- Utility functions --- #
+
+def word_is_found(grid, word, i, j, dx, dy) ->bool:
+    word_length = len(word)
+    rows = len(grid)
+    cols = len(grid[0])
+    # for each grid character check if it matches a word character in all directions
+    for k in range(word_length):
+        x = i + k*dx
+        y = j + k*dy
+        if not (0 <= x < rows and 0 <= y < cols) or (grid[x][y]!=word[k]):  # prevent index of being out of bounds
+            return False
+    return True
+
+
 
 
 #--- Main call --- #
