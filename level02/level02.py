8de4b2902578a2473f18f40586be116498164f79
Margot <margot.guillouard@gmail.com>
2025-04-16 12:17:05 +0200

    refactor: create utilities functions for level2 to simplfy code

--- level02/level02.py
+++ level02/level02.py
@@ -1,4 +1,4 @@
-## First part of advent of code day 2
+## Advent of code day 2
 
 # Objective : Compute the number of safe report(s) in the report list given as input of the problem
 # A report only counts as safe if both of the following are true:
@@ -12,63 +12,59 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.tools import process_reports
 
+# --- Main function --- #
+
 def level02_part1(input_path) -> int:
-    """function for computing the number of initial safe reports based on the 3 conditions"""
-    list_of_reports = process_reports(input_path)
-    safe_reports_count  = 0
-
-    for report in list_of_reports:
-        if ((verify_increasing(report) or verify_decreasing(report)) and verify_level_difference(report)):
-            safe_reports_count  +=1
-
-    return safe_reports_count 
+    return count_safe_reports(input_path, tolerate=False)
 
 def level02_part2(input_path) -> int:
-    """function for computing the number of new safe reports based on the condition tolerated"""
-    safe_reports_count  = 0
-    list_of_reports = process_reports(input_path)
-    new_safe_reports_count = 0
+    return count_safe_reports(input_path, tolerate=True)
 
-    for report in list_of_reports:
-        if ((verify_increasing(report) or verify_decreasing(report)) and verify_level_difference(report)):
-            safe_reports_count  +=1
-        # for each unsafe reports
-        # verify if at least one new pattern can be tolerated when removing one level at a time
-        elif(verify_tolerated_pattern(report)>=1):
-            new_safe_reports_count  +=1
-
-    return safe_reports_count + new_safe_reports_count
 
 # --- Reports verifications function --- #
 
-def verify_increasing(report):      
+def is_increasing(report) ->bool:      
     return np.all(report[1:] > report[:-1])
 
-
-def verify_decreasing(report):      
+def is_decreasing(report) ->bool:      
     return np.all(report[1:] < report[:-1])
 
-
-def verify_level_difference(report):    
+def has_valid_level_difference(report) ->bool:    
     level_difference = np.abs(report[1:] - report[:-1])
     return np.all((level_difference >= 1) & (level_difference <= 3))  
 
-def verify_tolerated_pattern(report):
+def is_safe(report) ->bool:
+    """Check if a report is safe based on increasing/decreasing and level difference."""
+    return (is_increasing(report) or is_decreasing(report)) and has_valid_level_difference(report)
+
+def count_tolerated_pattern(report) ->int:
     pattern = 0
     for level in range(len(report)):
         reduced_report = np.delete(report, level) #remove one level at a time
         #compute working number of patterns
-        if ((verify_increasing(reduced_report) or verify_decreasing(reduced_report)) and verify_level_difference(reduced_report)):
+        if is_safe(reduced_report):
             pattern += 1
     return pattern
+
+def count_safe_reports(input_path, tolerate=False) ->int:
+    reports_list = process_reports(input_path)
+    safe_report = 0
+
+    for report in reports_list:
+        if is_safe(report):
+            safe_report += 1
+        elif tolerate and count_tolerated_pattern(report) >= 1:
+            safe_report += 1
+
+    return safe_report
 
 
 # --- Main call --- #
 
 if __name__ == "__main__":
     file_path = "level02_input.txt"
-    result_part1 = part1(file_path)
-    result_part2 = part2(file_path)
+    result_part1 = level02_part1(file_path)
+    result_part2 = level02_part2(file_path)
     print("initial safe report count:", result_part1) 
     print("total safe report count:", result_part2) 
 
--- tests/test_level02.py
+++ tests/test_level02.py
@@ -5,23 +5,23 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from level02.level02 import verify_increasing, verify_decreasing, verify_level_difference, level02_part1, level02_part2
+from level02.level02 import is_increasing, is_decreasing, has_valid_level_difference, level02_part1, level02_part2
 
-def test_verify_increasing():
-    assert verify_increasing(np.array([1, 2, 3])) == True
-    assert verify_increasing(np.array([1, 1, 2])) == False
-    assert verify_increasing(np.array([3, 2, 1])) == False
+def test_is_increasing():
+    assert is_increasing(np.array([1, 2, 3])) == True
+    assert is_increasing(np.array([1, 1, 2])) == False
+    assert is_increasing(np.array([3, 2, 1])) == False
 
-def test_verify_decreasing():
-    assert verify_decreasing(np.array([5, 3, 1])) == True
-    assert verify_decreasing(np.array([5, 5, 4])) == False
-    assert verify_decreasing(np.array([1, 2, 3])) == False
+def test_is_decreasing():
+    assert is_decreasing(np.array([5, 3, 1])) == True
+    assert is_decreasing(np.array([5, 5, 4])) == False
+    assert is_decreasing(np.array([1, 2, 3])) == False
 
-def test_verify_level_difference():
-    assert verify_level_difference(np.array([1, 2, 3])) == True
-    assert verify_level_difference(np.array([1, 4])) == True
-    assert verify_level_difference(np.array([1, 5])) == False
-    assert verify_level_difference(np.array([1, 2, 6])) == False
+def has_valid_level_difference():
+    assert has_valid_level_difference(np.array([1, 2, 3])) == True
+    assert has_valid_level_difference(np.array([1, 4])) == True
+    assert has_valid_level_difference(np.array([1, 5])) == False
+    assert has_valid_level_difference(np.array([1, 2, 6])) == False
 
 def test_full_example(tmp_path):
     # Exemple given in the advend of code
